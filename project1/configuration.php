eb66e8a3903e5c5a2d395412473c4db3faa3f08b
vennilass <39242715+vennilass@users.noreply.github.com>
2018-07-17 19:45:38 +0530

    Create configuration.php

---
+++ project1/configuration.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_NAME','library');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if(!$link)
+{
+die('Could not connect:'.mysql_error());
+}
+$db_selected = mysql_select_db(DB_NAME, $link);
+if(!$db_selected)
+{
+die('cant use'.DB_NAME.':'.mysql_error());
+}
