57b192fcd5d4b990069324baf64d02a62f3accaa
vennilass <39242715+vennilass@users.noreply.github.com>
2018-07-17 19:51:58 +0530

    Create learn.php

---
+++ project1/learn.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8"/>
+ <title>Confirmation boxes</title>
+
+</head>
+<body>
+
+ <script type="text/javascript">
+ 
+ // This is a confirmation box which gives two options: OK, and Cancel.
+ var r=confirm("Press a button");
+ if (r==true) {
+ x = "You pressed OK!";
+ } else {
+ x = "You pressed Cancel!";
+ }
+ document.write(x); 
+ 
+ 
+ </script>
+
+</body
+</html>
